574127ef446aaae4f65c1eacc7b1b99f9f98fbbc
Fernando Oliveira do Nascimento <88868670+Nnando0220@users.noreply.github.com>
2024-10-22 05:52:03 -0300

    Atualizar o app.js

--- src/app.js
+++ src/app.js
@@ -2,14 +2,12 @@ const express = require('express');
 require('dotenv').config();
 const routerUser  = require('./Routers/userRouter.js');
 const routerResource = require('./Routers/resourceRouter.js');
-const routerScheduling = require('./Routers/schedulingRouter.js');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger/swagger.json'); 
+const routerScheduling = require('./Routers/schedulingRouter.js'); 
 
 const app = express();
 app.use(express.json());
 app.use(routerUser, routerResource, routerScheduling);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 
 module.exports = app;
